979d710ec2f11936b9ba055acf5175a2e486ee70
Toytum <steath1999@gmail.com>
2020-04-19 23:27:34 -0700

    Adds ticket and user tests

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
-
+    it "exists" do
+        ticket= Ticket.new
+    end
+    it {belongs_to(:region)}
+    it {belongs_to(:resource_category)}
 end
