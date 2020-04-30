383be1336c399fe50abd7d36bb8b3286c060ab53
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-04-30 14:43:05 -0700

    Complete coverage of Organization attributes References #1

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -4,6 +4,17 @@ RSpec.describe Organization, type: :model do
   let(:organization) { Organization.new }
 
   describe "Attributes" do
-    it {should respond_to(:name) }
+    it { should respond_to(:name) }
+    it { should respond_to(:status) }
+    it { should respond_to(:email) }
+    it { should respond_to(:phone) }
+    it { should respond_to(:description) }
+    it { should respond_to(:rejection_reason) }
+    it { should respond_to(:liability_insurance) }
+    it { should respond_to(:primary_name) }
+    it { should respond_to(:secondary_name) }
+    it { should respond_to(:secondary_phone) }
+    it { should respond_to(:title) }
+    it { should respond_to(:transportation) }
   end
 end
