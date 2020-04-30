319c9a4afe22a75b402941d5f20c5949bc0a33f4
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-04-30 15:19:40 -0700

    Validate uniqueness of email References #1

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -32,8 +32,12 @@ RSpec.describe Organization, type: :model do
     it { should validate_presence_of(:primary_name) }
     it { should validate_presence_of(:secondary_name) }
     it { should validate_presence_of(:secondary_phone) }
+    
     it { should validate_length_of(:email).is_at_least(1).is_at_most(255) }
+    
     it { should allow_value("fake@example.com").for(:email) }
     it { should_not allow_value("fake").for(:email) }
+
+    it { should validate_uniqueness_of(:email).case_insensitive }
   end
 end
