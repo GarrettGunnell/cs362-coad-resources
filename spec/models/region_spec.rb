a96200c7bcf5c72d90e81fb1f02f25466d4f1b7d
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-05-01 22:27:35 -0700

    Describe Region validations References #2

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -10,4 +10,10 @@ RSpec.describe Region, type: :model do
   describe "Relationships" do
     it { should have_many(:tickets) }
   end
+
+  describe "Validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(1).is_at_most(255) }
+    it { should validate_uniqueness_of(:name).case_insensitive }
+  end
 end
