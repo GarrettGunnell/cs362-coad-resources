4507d14fd429c210e9ae497fd4d2d9989aab8a80
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-05-02 05:11:57 -0700

    Describe ResourceCategory validations References #3

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -13,4 +13,9 @@ RSpec.describe ResourceCategory, type: :model do
     it { should have_many(:tickets) }
   end
   
+  describe "Validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(1).is_at_most(255) }
+    it { should validate_uniqueness_of(:name).case_insensitive }
+  end
 end
