bdf1303e3f5436d9b029ee87fbb00ee735720ca5
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-05-02 06:10:50 -0700

    Reformat code to be consistent References #5

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,25 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) {build(:user)}
+  let(:user) { build(:user) }
 
   describe "Attributes" do
-    it {should respond_to(:email)}
-    it {should respond_to(:password)}
-    it {should respond_to(:role)}
+    it { should respond_to(:email) }
+    it { should respond_to(:password) }
+    it { should respond_to(:role) }
   end
 
   describe "Relationships" do
-    it {should belong_to(:organization)}
+    it { should belong_to(:organization) }
   end
 
   describe "Validations" do
-    it {should validate_presence_of(:email)}
-    it {should validate_length_of(:email).is_at_least(1).is_at_most(255)}
+    it { should validate_presence_of(:email) }
+    it { should validate_length_of(:email).is_at_least(1).is_at_most(255) }
     # it {should validateFORMAT?}
-    it {should validate_uniqueness_of(:email).case_insensitive}
-    it {should validate_presence_of(:password)}
-    it {should validate_length_of(:password).is_at_least(7).is_at_most(255)}
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should validate_presence_of(:password) }
+    it { should validate_length_of(:password).is_at_least(7).is_at_most(255) }
   end
 
   
