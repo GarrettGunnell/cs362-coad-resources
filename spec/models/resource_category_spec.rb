1d2eea8f565bb12b9ba2e4403ec79494aec76c04
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-05-02 01:13:36 -0700

    Describe ResourceCategory attributes References #3

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
+  let(:resourcecategory) { build(:resourcecategory) }
 
+  describe "Attributes" do
+    it { should respond_to(:name) }
+    it { should respond_to(:active) }
+  end
   
 end
