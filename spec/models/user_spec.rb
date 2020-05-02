27faa17eabe8032b96ee949287283efe603d06a7
Toytum <steath1999@gmail.com>
2020-05-02 13:23:19 -0700

    Adds more attribute tests to User that were missed before

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,16 @@ RSpec.describe User, type: :model do
 
   describe "Attributes" do
     it { should respond_to(:email) }
-    it { should respond_to(:password) }
+    it { should respond_to(:encrypted_password) }
+    it { should respond_to(:reset_password_token) }
+    it { should respond_to(:reset_password_sent_at) }
+    it { should respond_to(:remember_created_at) }
+    it { should respond_to(:confirmation_token) }
+    it { should respond_to(:confirmed_at) }
+    it { should respond_to(:confirmation_sent_at) }
+    it { should respond_to(:unconfirmed_email) }
     it { should respond_to(:role) }
+    it { should respond_to(:organization_id) }
   end
 
   describe "Relationships" do
