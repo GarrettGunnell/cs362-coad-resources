7d048151a0dd72fd9b751161c0bdebe28d4379a1
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-05-05 09:28:50 -0700

    Remove unnecessary comments References #5

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,7 +24,6 @@ RSpec.describe User, type: :model do
   describe "Validations" do
     it { should validate_presence_of(:email) }
     it { should validate_length_of(:email).is_at_least(1).is_at_most(255) }
-    # it {should validateFORMAT?}
     it { should validate_uniqueness_of(:email).case_insensitive }
     it { should validate_presence_of(:password) }
     it { should validate_length_of(:password).is_at_least(7).is_at_most(255) }
