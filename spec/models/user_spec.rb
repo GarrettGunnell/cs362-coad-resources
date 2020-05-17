797cb26cf9248b2a06db247eeb8d9e323f074be2
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-05-17 02:19:39 -0700

    Add additional User validation tests References #5

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,6 +24,8 @@ RSpec.describe User, type: :model do
   describe "Validations" do
     it { should validate_presence_of(:email) }
     it { should validate_length_of(:email).is_at_least(1).is_at_most(255) }
+    it { should allow_value("fake@example.com").for(:email) }
+    it { should_not allow_value("fake").for(:email) }
     it { should validate_uniqueness_of(:email).case_insensitive }
     it { should validate_presence_of(:password) }
     it { should validate_length_of(:password).is_at_least(7).is_at_most(255) }
