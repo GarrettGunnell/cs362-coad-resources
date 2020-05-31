d063a1ea425ff8a8303a9f5078f1108cffad3b72
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-05-31 16:47:24 -0700

    Describe non logged-in region redirections

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
+  
+  context 'As a non logged-in user' do
+    describe 'GET #index' do
+      specify { expect(get(:index)).to redirect_to new_user_session_path }
+    end
 
+    describe 'GET #new' do
+      specify { expect(get(:new)).to redirect_to new_user_session_path }
+    end
+
+    describe 'POST #create' do
+      specify { expect(post(:create)).to redirect_to new_user_session_path }
+    end
+
+    describe 'GET #edit' do
+      specify { expect(get(:edit, params: {id: 'FAKE'})).to redirect_to new_user_session_path }
+    end
+
+    describe 'PATCH #update' do
+      specify { expect(patch(:update, params: {id: 'FAKE'})).to redirect_to new_user_session_path }
+    end
+
+    describe 'PUT #update' do
+      specify { expect(put(:update, params: {id: 'FAKE'})).to redirect_to new_user_session_path }
+    end
+
+    describe 'GET #show' do
+      specify { expect(get(:show, params: {id: 'FAKE'})).to redirect_to new_user_session_path }
+    end
+
+    describe 'POST #destroy' do
+      specify { expect(post(:destroy, params: {id: 'FAKE'})).to redirect_to new_user_session_path }
+    end
+  end
 end
