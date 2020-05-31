a3845173322219cb7e56f17651c6a20ce0a0343c
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-05-31 16:36:42 -0700

    Describe dashboard controller redirections

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -2,4 +2,27 @@ require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
 
+  context "as a non logged-in user" do
+    describe 'GET #index' do
+      specify { expect(get(:index)).to redirect_to new_user_session_path }
+    end
+  end
+
+  context "as a logged-in user" do
+    let(:user) { create(:user) }
+    before(:each) { sign_in(user) }
+
+    describe 'GET #index' do
+      specify { expect(get(:index)).to be_successful }
+    end
+  end
+
+  context "as an admin user" do
+    let(:user) { create(:user, :admin) }
+    before(:each) { sign_in(user) }
+
+    describe 'GET #index' do
+      specify { expect(get(:index)).to be_successful }
+    end
+  end
 end
