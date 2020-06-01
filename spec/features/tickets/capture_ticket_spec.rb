b3e6a78ee6b8060ebe2c0814c58fcf22443336d8
Toytum <steath1999@gmail.com>
2020-05-31 22:20:01 -0700

    Adds users feature tests for both files

--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -2,11 +2,21 @@ require 'rails_helper'
 
 RSpec.describe 'Capturing a ticket', type: :feature do
 
-  before :each do
-      @user = create(:user, :admin)
-      @organization = create(:organization, :submitted)
-  end
+#   before :each do
+#       @user = create(:user)
+#       @organization = create(:organization, :approved)
+#       @ticket = create(:ticket)
+#   end
 
-  it "displays " do
-  end
+#   it "displays organization after capture" do
+#     log_in_as @user
+#     visit dashboard_path
+#     byebug
+#     click_on 'Tickets'
+#     click_on(@ticket.name)
+#     click_on 'Capture'
+#     click_on 'Tickets'
+#     click_on(@ticket.name)
+#     expect(page).to have_content('Release')
+#   end
 end
--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
-    
+  before :each do
+    @user = create(:user)
+  end
+
+  it "displays option for log out after sign in" do
+    visit root_path
+    click_on 'Log in'
+    fill_in 'Email address', with: (@user.email)
+    fill_in 'Password', with: (@user.password)
+    click_on 'Sign in'
+    expect(page).to have_content('Log out')
+  end
 end
