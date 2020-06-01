61117d76865c4ec2038c131c620ed6d4849b62fa
Toytum <steath1999@gmail.com>
2020-05-31 22:26:20 -0700

    adds test for capture ticket feature

--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -2,21 +2,21 @@ require 'rails_helper'
 
 RSpec.describe 'Capturing a ticket', type: :feature do
 
-#   before :each do
-#       @user = create(:user)
-#       @organization = create(:organization, :approved)
-#       @ticket = create(:ticket)
-#   end
+  before :each do
+      @user = create(:user)
+      @user.organization = create(:organization, :approved)
+      @user.save
+      @ticket = create(:ticket)
+  end
 
-#   it "displays organization after capture" do
-#     log_in_as @user
-#     visit dashboard_path
-#     byebug
-#     click_on 'Tickets'
-#     click_on(@ticket.name)
-#     click_on 'Capture'
-#     click_on 'Tickets'
-#     click_on(@ticket.name)
-#     expect(page).to have_content('Release')
-#   end
+  it "displays ticket in organization dashboard after capture" do
+    log_in_as @user
+    visit dashboard_path
+    click_on 'Tickets'
+    click_on(@ticket.name)
+    click_on 'Capture'
+    click_on 'Tickets'
+    click_on(@ticket.name)
+    expect(page).to have_content('Release')
+  end
 end
