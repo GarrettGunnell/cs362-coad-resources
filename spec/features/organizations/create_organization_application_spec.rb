07f1b17ac3816d80e58db9e6a28943913bde4c47
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-05-31 23:02:20 -0700

    placeholder commit

--- spec/features/organizations/create_organization_application_spec.rb
+++ spec/features/organizations/create_organization_application_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'Creating an Organization Application', type: :feature do
 
+    before :each do
+        @user = create(:user)
+    end
+
+    it "displays an application submitted message" do
+        log_in_as @user
+        visit new_organization_application_path
+        
+    end
 end
