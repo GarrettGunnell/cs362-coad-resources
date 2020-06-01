d2d94759352a2e1243da96bd862b2872b532ee84
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-05-31 17:00:03 -0700

    Describe logged-in user region redirections

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -35,4 +35,37 @@ RSpec.describe RegionsController, type: :controller do
       specify { expect(post(:destroy, params: {id: 'FAKE'})).to redirect_to new_user_session_path }
     end
   end
+
+  context 'As a logged-in user' do
+    let(:user) { create(:user) }
+    before(:each) { sign_in(user) }
+
+    describe 'GET #index' do
+      specify { expect(get(:index)).to redirect_to dashboard_path }
+    end
+
+    describe 'GET #new' do
+      specify { expect(get(:new)).to redirect_to dashboard_path }
+    end
+
+    describe 'POST #create' do
+      specify { expect(post(:create)).to redirect_to dashboard_path }
+    end
+
+    describe 'GET #edit' do
+      specify { expect(get(:edit, params: {id: 'FAKE'})).to redirect_to dashboard_path }
+    end
+
+    describe 'PATCH #update' do
+      specify { expect(patch(:update, params: {id: 'FAKE'})).to redirect_to dashboard_path }
+    end
+
+    describe 'GET #show' do
+      specify { expect(get(:show, params: {id: 'FAKE'})).to redirect_to dashboard_path }
+    end
+
+    describe 'POST #destroy' do
+      specify { expect(post(:destroy, params: {id: 'FAKE'})).to redirect_to dashboard_path }
+    end
+  end
 end
