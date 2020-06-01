a5e675281a060318bad98d0835e6a46726520579
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-05-31 18:20:49 -0700

    Describe admin user resource category redirections

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -31,19 +31,19 @@ RSpec.describe ResourceCategoriesController, type: :controller do
       specify { expect(get(:show, params: {id: 'FAKE'})).to redirect_to new_user_session_path }
     end
 
-    describe 'POST #activate' do
-      specify { expect(post(:activate, params: {id: 'FAKE'})).to redirect_to new_user_session_path }
+    describe 'PATCH #activate' do
+      specify { expect(patch(:activate, params: {id: 'FAKE'})).to redirect_to new_user_session_path }
     end
 
-    describe 'POST #deactivate' do
-      specify { expect(post(:deactivate, params: {id: 'FAKE'})).to redirect_to new_user_session_path }  
+    describe 'PATCH #deactivate' do
+      specify { expect(patch(:deactivate, params: {id: 'FAKE'})).to redirect_to new_user_session_path }  
     end
   end
 
   context 'As a logged-in user' do
     let(:user) { create(:user) }
     before(:each) { sign_in(user) }
-    
+
     describe 'GET #index' do
       specify { expect(get(:index)).to redirect_to dashboard_path }
     end
@@ -64,20 +64,68 @@ RSpec.describe ResourceCategoriesController, type: :controller do
       specify { expect(patch(:update, params: {id: 'FAKE'})).to redirect_to dashboard_path }
     end
 
-    describe 'PUT #update' do
-      specify { expect(put(:update, params: {id: 'FAKE'})).to redirect_to dashboard_path }
-    end
-
     describe 'GET #show' do
       specify { expect(get(:show, params: {id: 'FAKE'})).to redirect_to dashboard_path }
     end
 
-    describe 'POST #activate' do
-      specify { expect(post(:activate, params: {id: 'FAKE'})).to redirect_to dashboard_path }
+    describe 'PATCH #activate' do
+      specify { expect(patch(:activate, params: {id: 'FAKE'})).to redirect_to dashboard_path }
     end
 
-    describe 'POST #deactivate' do
-      specify { expect(post(:deactivate, params: {id: 'FAKE'})).to redirect_to dashboard_path }  
+    describe 'PATCH #deactivate' do
+      specify { expect(patch(:deactivate, params: {id: 'FAKE'})).to redirect_to dashboard_path }  
+    end
+  end
+
+  context 'As an admin user' do
+    let(:user) { create(:user, :admin) }
+    before(:each) { sign_in(user) }
+    
+    describe 'GET #index' do
+      specify { expect(get(:index)).to be_successful }
+    end
+
+    describe 'GET #new' do
+      specify { expect(get(:new)).to be_successful }
+    end
+
+    describe 'POST #create' do
+    specify { expect(post(:create, params: { resource_category: attributes_for(:resourcecategory) })).to redirect_to resource_categories_path }
+    end
+
+    describe 'GET #edit' do
+      specify { 
+        resource_category = create(:resourcecategory)
+        expect(get(:edit, params: {id: resource_category.id})).to be_successful
+      }
+    end
+
+    describe 'PATCH #update' do
+      specify { 
+        resource_category = create(:resourcecategory)
+        expect(patch(:update, params: {id: resource_category.id, resource_category: attributes_for(:resourcecategory) })).to redirect_to resource_category
+      }
+    end
+
+    describe 'GET #show' do
+      specify { 
+        resource_category = create(:resourcecategory)
+        expect(get(:show, params: {id: resource_category.id})).to be_successful
+       }
+    end
+
+    describe 'PATCH #activate' do
+      specify { 
+        resource_category = create(:resourcecategory, :inactive)
+        expect(patch(:activate, params: {id: resource_category.id})).to redirect_to resource_category
+       }
+    end
+
+    describe 'PATCH #deactivate' do
+      specify { 
+        resource_category = create(:resourcecategory, :active)
+        expect(patch(:deactivate, params: { id: resource_category.id })).to redirect_to resource_category
+      }  
     end
   end
 end
