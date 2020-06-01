6dd4820d484e64db63af014f8895a45cdc2f16ec
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-05-31 23:50:18 -0700

    Describe creating a region

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
-    
+    before :each do
+        @user = create(:user, :admin)
+      end
+  
+      it "displays a success notification" do
+        log_in_as @user
+        visit '/regions/new'
+        fill_in 'Name', with: 'Fake Region Name'
+        click_on 'Add Region'
+        expect(page).to have_text('Region successfully created')
+      end
 end
