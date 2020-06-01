d582d3dcea85da3596544d86a3568b651d6c3e61
Toytum <steath1999@gmail.com>
2020-05-31 22:59:31 -0700

    adds deleting region feature test

--- spec/features/regions/admin_deletes_region_spec.rb
+++ spec/features/regions/admin_deletes_region_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Region', type: :feature do
+  before :each do
+    @user = create(:user, :admin)
+    @region = create(:region)
+  end
 
+  it "does not display region after deletion" do
+    log_in_as @user
+    click_on 'Region'
+    click_on(@region.name)
+    click_on 'Delete'
+    expect(page).to have_content(@region.name + ' was deleted')
+  end
 end
