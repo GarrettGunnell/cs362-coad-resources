0d4de86eb4bec1fe10a225017b4bdfae2efe071f
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-05-31 23:48:17 -0700

    Describe rejecting an organization

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Rejecting an organization', type: :feature do
+    before :each do
+        @user = create(:user, :admin)
+        @organization = create(:organization, :submitted)
+    end
 
+    it "Displays an organization rejected message" do
+        log_in_as @user
+        visit organizations_path
+        click_on 'Review'
+        click_on 'Reject'
+        expect(page).to have_text('has been rejected')
+    end
 end
